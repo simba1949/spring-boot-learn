57827bd01203a34996e46dcd5c6f5aa6cd6e62fb
anthony <anthony1949@zto.com>
2023-04-03 16:08:15 +0800

    feature：springboot配置banner

--- spring-boot-get-start/src/main/java/top/simba1949/SpringBootGetStartApplication.java
+++ spring-boot-get-start/src/main/java/top/simba1949/SpringBootGetStartApplication.java
@@ -1,7 +1,9 @@
 package top.simba1949;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import top.simba1949.config.ConfigurationPropertiesAndEnableConfig;
 
@@ -12,7 +14,17 @@ import top.simba1949.config.ConfigurationPropertiesAndEnableConfig;
 @EnableConfigurationProperties(value = {ConfigurationPropertiesAndEnableConfig.class})
 @SpringBootApplication
 public class SpringBootGetStartApplication {
+//	public static void main(String[] args) {
+//		SpringApplication application = new SpringApplication(SpringBootGetStartApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF); // 关闭 Banner
+//		application.run(args);
+//	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGetStartApplication.class, args);
+		new SpringApplicationBuilder()
+				.sources(SpringBootGetStartApplication.class) // 项目中的启动类
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
 	}
+
 }
