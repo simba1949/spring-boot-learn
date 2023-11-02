234c4da078f8abaca97e596146dc54e3887b4656
anthony <anthony1949@zto.com>
2023-11-02 15:11:36 +0800

    feature：spring mvc 配置静态资源

--- spring-boot-servlet-web/src/main/java/vip/openpark/servletWeb/config/WebMvcConfig.java
+++ spring-boot-servlet-web/src/main/java/vip/openpark/servletWeb/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package vip.openpark.servletWeb.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import vip.openpark.servletWeb.interceptor.UserInterceptor;
 
@@ -22,5 +23,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addConverter(new DateTimeStrToTimestampConverter());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 第一个方法设置访问路径前缀，第二个方法设置资源路径
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
 	}
 }
