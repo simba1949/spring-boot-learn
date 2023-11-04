03331d835cd572f04f682ceb55ac9ea87961df0e
simba1949 <simba1949@outlook.com>
2023-11-04 15:41:51 +0800

    feature：@ControllerAdvice 基本使用

--- spring-boot-servlet-web/src/main/java/vip/openpark/servletWeb/exception/GlobalException.java
+++ spring-boot-servlet-web/src/main/java/vip/openpark/servletWeb/exception/GlobalException.java
@@ -1,5 +1,6 @@
 package vip.openpark.servletWeb.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.io.IOException;
 
 /**
- * @ControllerAdvice 全局异常统一处理类标识
+ * @ControllerAdvice 全局异常统一处理类标识，也可以指定类或者包
  *
  * @author anthony
  * @version 2019/7/8 10:22
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalException {
 	/**
@@ -22,14 +24,14 @@ public class GlobalException {
 	@ResponseBody
 	@ExceptionHandler(value = IOException.class)
 	public String ioExceptionDealWith(IOException e){
-		System.out.println(e.getMessage());
+		log.info("捕捉到IO异常，异常信息：{}", e.getMessage(), e);
 		return e.getMessage();
 	}
 
 	@ResponseBody
 	@ExceptionHandler(value = Exception.class)
 	public String exceptionDealWith(Exception e){
-		System.out.println(e.getMessage());
+		log.info("捕捉到除IO异常之外的异常，异常信息：{}", e.getMessage(), e);
 		return e.getMessage();
 	}
 }
