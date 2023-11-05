07cdf4eb6b4da2a08e5e5fcbab2c6418926ace15
simba1949 <simba1949@outlook.com>
2023-11-05 17:50:50 +0800

    feature：spring-boot-starter-web 更替 web 容器

--- spring-boot-servlet-container/src/main/java/vip/openpark/servlet/container/ServletContainerApplication.java
+++ spring-boot-servlet-container/src/main/java/vip/openpark/servlet/container/ServletContainerApplication.java
@@ -1,4 +1,4 @@
-package vip.openpark;
+package vip.openpark.servlet.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 2019/7/9 8:39
  */
 @SpringBootApplication
-public class Application {
+public class ServletContainerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ServletContainerApplication.class, args);
 	}
 }
--- spring-boot-servlet-container/src/main/java/vip/openpark/servlet/container/controller/HelloController.java
+++ spring-boot-servlet-container/src/main/java/vip/openpark/servlet/container/controller/HelloController.java
@@ -1,4 +1,4 @@
-package vip.openpark.test.controller;
+package vip.openpark.servlet.container.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
